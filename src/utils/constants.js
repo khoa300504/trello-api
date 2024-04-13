060c7ce0e4e33cfdef74b9ec096283cf3b1610d7
khoa300504 <khoa300504@gmail.com>
2024-04-13 09:54:04 +0700

    Update WhiteList Domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 //Domains has permission to access server resource
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173'
+  'https://trello-web-liart-eight.vercel.app/'
 ]
 
 export const BOARD_TYPES = {
